48b127e310cd69361daf514e66e41e53b95c40ba
Vincent <vincent.becerra.720@my.csun.edu>
2020-05-04 22:18:06 -0700

    spaces

--- scripts/bastion-userdata.sh
+++ scripts/bastion-userdata.sh
@@ -2,10 +2,10 @@
 
 sudo adduser calstatepays || true
 
-sudo -i -u calstatepays mkdir /home/calstatepays /.ssh || true
-sudo -i -u calstatepays chmod 700 /home/calstatepays /.ssh || true
-sudo -i -u calstatepays touch /home/calstatepays /.ssh/authorized_keys || true
-sudo -i -u calstatepays chmod 600 /home/calstatepays /.ssh/authorized_keys || true
+sudo -i -u calstatepays mkdir /home/calstatepays/.ssh || true
+sudo -i -u calstatepays chmod 700 /home/calstatepays/.ssh || true
+sudo -i -u calstatepays touch /home/calstatepays/.ssh/authorized_keys || true
+sudo -i -u calstatepays chmod 600 /home/calstatepays/.ssh/authorized_keys || true
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUjg3fHlAf2NrQg/VB4B76bE3ls8Zqdvbyw/FTgXWOevM+HtZOaZkfRKdB8OwW4x01gwPf5kRSl0lDUModOHXn4ItKSFnTACdJA2zgmqUv9HGqNVgQpVNgkvQ2MN7G4rPpINwcJv/EhaRTiBA1wFJvTVf7ifKwlXK3E1HoOpZTIjYMqa0kAw5FFxHrQW0V8KlFb1Rto7KG3KDKDs2Tgu0LYEUtQy+YM3RUKwLHTZbE+Z80E3sfrpTznYsWGQfv8UkpF7ET35TCS5dofEGmm8uGEjNs/X59xs8PWoIp/UyFaAJfZfH1b33MEkUX+oEXRcLMpIvZCtI9Xnb3N3pQOjLR vibe@LoopyD" >> ~/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCz5jJHqVKZXBYUjJvEo5PkqQ8qV3c9SsrHgxWYwGhCvCI5L7fcDRF+jvdWpX7RvXSedl0M053q9igVsNFRFde4Y2mYsvlHqQIPBCca6nFBFBzzGEzAKzN1jFCPnASndA9wCjPQSmSKpBJr4jCc0yU5UNAW0iqhycjtPpqpsF17mOWGsKCFWvM5ZpSXVuAWWmWlNSd3fhdBeo6liqB92SAoTjmO/jfLbVYb78gRfUate8iK5qJfiN0C6/NaktRd/32rCnd26GP4SQQQ9Bhh/tV82UHj810cAqNZTl5MGkKwfxPsBP5t5p6jEwSg7dTlGoTgT5urBbt5Ngyxrb+KJOLQ0Ki4YKopfi5MwSiIOwNOGx4dsAc3xIMmAUBOqilAJW2B7P5DzEk8nSSogbfqiQTMxasQKaw5kQNXLulBDQcql022g9GcXzf7+XKn8KSCnT3w2yLmwdqqb5+rdQfTicaJz3v1cQZvQzpFMbCZA5E/71BMNqJ2bvRjrLvTnDqCbWM= edgargodoy@Edgars-MacBook-Pro.local" >> ~/.ssh/authorized_keys
