0ddee01d6b38b4f34168f40c451ac9c87f0de33a
Vincent <vincent.becerra.720@my.csun.edu>
2020-05-04 20:41:17 -0700

    deletions

--- scripts/spot-asg-userdata.sh
+++ scripts/spot-asg-userdata.sh
@@ -3,8 +3,6 @@ set -eu -o pipefail
 sudo yum install httpd -y
 sudo service httpd restart
 echo “hello from $(curl http://169.254.169.254/latest/meta-data/instance-id)” | sudo tee  /var/www/html/index.html  > /dev/null
-service httpd start
 sudo yum repolist all
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum install stress -y
 stress --cpu 1 --timeout 300
