9adfdfd06dba1ab45b16c30e595dd26572ac40b3
Vincent <vincent.becerra.720@my.csun.edu>
2020-05-04 20:47:49 -0700

    change stress repo

--- scripts/spot-asg-userdata.sh
+++ scripts/spot-asg-userdata.sh
@@ -3,6 +3,6 @@ set -eu -o pipefail
 sudo yum install httpd -y
 sudo service httpd restart
 echo “hello from $(curl http://169.254.169.254/latest/meta-data/instance-id)” | sudo tee  /var/www/html/index.html  > /dev/null
-sudo yum repolist all
+sudo amazon-linux-extras install epel
 sudo yum install stress -y
 stress --cpu 1 --timeout 300
