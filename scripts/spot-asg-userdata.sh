84884298a0fb44ad4cd495c1ad3213263e93b6dd
Vincent <vincent.becerra.720@my.csun.edu>
2020-05-04 20:10:06 -0700

    sudo tee

--- scripts/spot-asg-userdata.sh
+++ scripts/spot-asg-userdata.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 sudo yum install httpd -y
 sudo service httpd restart
-sudo echo “hello world from $instanceid” > /var/www/html/index.html
+echo “hello from $instanceid” | sudo tee  /var/www/html/index.html  > /dev/null
 service httpd start
 sudo yum repolist all
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
